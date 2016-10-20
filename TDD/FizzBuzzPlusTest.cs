64ad82881dd52113d1279c1aeaaaa1ba4b3bf0aa
Milos Davidovic <milosh.davidovic@yahoo.com>
2016-10-20 20:12:11 +0200

    Impemented FuzzBuzzPlus

---
+++ TDD/FizzBuzzPlusTest.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    [TestClass]
+    public class FizzBuzzPlusTest
+    {
+        [TestMethod]
+        public void FuzzBuzzPlus_ForDividableByThree_ReturnFuzz()
+        {
+            var fuzzBuzzer = FuzzBuzzPlus.GetFuzzBuzzer();
+            Dictionary<int, object> retvalue = fuzzBuzzer.DoFuzzBuzzPlus(100);
+            Assert.AreEqual( "Fuzz", retvalue[3]);
+            Assert.AreEqual("Fuzz", retvalue[33]);
+            Assert.AreEqual("Fuzz", retvalue[63]);
+        }
+
+        [TestMethod]
+        public void FuzzBuzzPlus_ForDevidableAndContainingThree_ReturnFuzz()
+        {
+            var fuzzBuzzer = FuzzBuzzPlus.GetFuzzBuzzer();
+            Dictionary<int, object> retvalue = fuzzBuzzer.DoFuzzBuzzPlus(100);
+            Assert.AreEqual("Fuzz", retvalue[34]);
+            Assert.AreEqual("Fuzz", retvalue[23]);
+            Assert.AreEqual("Fuzz", retvalue[53]);
+        }
+
+        [TestMethod]
+        public void FuzzBuzzPlus_ForDevidableByFiveOrContainingFive()
+        {
+            var fuzzBuzzer = FuzzBuzzPlus.GetFuzzBuzzer();
+            Dictionary<int, object> retvalue = fuzzBuzzer.DoFuzzBuzzPlus(100);
+            Assert.AreEqual("Buzz", retvalue[5]);
+            Assert.AreEqual("Buzz", retvalue[52]);
+            Assert.AreEqual("Buzz", retvalue[95]);
+        }
+    }
+
+    public class FuzzBuzzPlus
+    {
+        public static FuzzBuzzPlus GetFuzzBuzzer()
+        {
+            return new FuzzBuzzPlus();
+        }
+
+        public Dictionary<int, object> DoFuzzBuzzPlus(int number)
+        {
+            Dictionary<int, object> retvalue = new Dictionary<int, object>() { };
+            for (int i = 1; i < number + 1; i++)
+            {
+                if (IsFuzz(i) || IsContainingDigitThree(i))
+                {
+                    retvalue.Add(i, "Fuzz");
+                }
+                else if (IsBuzz(i) || IsContainingDigitFive(i))
+                {
+                    retvalue.Add(i, "Buzz");
+                }
+                else
+                {
+                    retvalue.Add(i, i);
+                }
+            }
+            return retvalue;
+        }
+
+        private bool IsBuzz(int i)
+        {
+            return i % 5 == 0;
+        }
+
+        private bool IsContainingDigitFive(int i)
+        {
+            return i.ToString().Contains("5");
+        }
+
+        private static bool IsContainingDigitThree(int i)
+        {
+            return i.ToString().Contains("3");
+        }
+
+        private bool IsFuzz(int number)
+        {
+            return number % 3 == 0;
+        }
+    }
+}
---
+++ TDD/FizzBuzzTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void FizzBuzz_ForDefinedNuberOfIterations_ReturnsArrayOfNumberOfIterationsSize()
+        {
+            var fizzBuzzer = FizzBuzz.GetFizzBuzzer();
+            var numberOfLinesPrinted = fizzBuzzer.DoFizzBuzz(20);
+            Assert.AreEqual(numberOfLinesPrinted.Count(), 20);
+        }
+        [TestMethod]
+        public void FizzBuzz_ForMultipleOfThree_ReturnsFizzOnRightPlaces()
+        {
+            var fizzBuzzer = FizzBuzz.GetFizzBuzzer();
+            List<object> fuzzArray = fizzBuzzer.DoFizzBuzz(100);
+            Assert.AreEqual("Fizz", (string)fuzzArray[2]);
+            Assert.AreEqual("Fizz", (string)fuzzArray[8]);
+            Assert.AreEqual("Fizz", (string)fuzzArray[26]);
+        }
+        [TestMethod]
+        public void FizzBuzz_ForMultipleOfFive_ReturnsBuzzOnRightPlaces()
+        {
+            var fizzBuzzer = FizzBuzz.GetFizzBuzzer();
+            List<object> fuzzArray = fizzBuzzer.DoFizzBuzz(100);
+            Assert.AreEqual("Buzz", (string)fuzzArray[4]);
+            Assert.AreEqual("Buzz", (string)fuzzArray[99]);
+            Assert.AreEqual("Buzz", (string)fuzzArray[24]);
+        }
+
+        [TestMethod]
+        public void FizzBuzz_ForMultipleOfFiveAndThree_ReturnsFizzBuzzOnRightPlaces()
+        {
+            var fizzBuzzer = FizzBuzz.GetFizzBuzzer();
+            List<object> fuzzArray = fizzBuzzer.DoFizzBuzz(100);
+            Assert.AreEqual("FizzBuzz", (string)fuzzArray[14]);
+            Assert.AreEqual("FizzBuzz", (string)fuzzArray[29]);
+            Assert.AreEqual("FizzBuzz", (string)fuzzArray[59]);
+        }
+
+    }
+
+     class FizzBuzz
+    {
+        public FizzBuzz()
+        {
+
+        }
+
+        public static FizzBuzz GetFizzBuzzer()
+        {
+            return new FizzBuzz();
+        }
+
+        public List<object> DoFizzBuzz(int nuberOfIterations)
+        {
+            List<object> retList = new List<object>();
+            for (int i = 1; i < nuberOfIterations + 1; i++)
+            {
+                if (IsFizzBuzz(i))
+                {
+                    retList.Add("FizzBuzz");
+                }
+                else if (IsFizz(i))
+                {
+                    retList.Add("Fizz");
+                }
+                else if (IsBuzz(i))
+                {
+                    retList.Add("Buzz");
+                }
+                else
+                {
+                    retList.Add(i);
+                }
+            }
+            return retList;
+        }
+
+        private static bool IsBuzz(int i)
+        {
+            return i % 5 == 0;
+        }
+
+        private static bool IsFizz(int i)
+        {
+            return i % 3 == 0;
+        }
+
+        private static bool IsFizzBuzz(int i)
+        {
+            return i % 3 == 0 && i % 5 == 0;
+        }
+    }
+}
