aa1c279e70aa532e3d46f0f237519d0f10484f7e
Milos Davidovic <milosh.davidovic@yahoo.com>
2016-10-26 23:28:56 +0200

    added some katas

---
+++ TDD/CombinedNumberTest.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    [TestClass]
+    public class CombinedNumberTest
+    {
+        [TestMethod]
+        public void ForArrayWithNegativeNumber_FindCominedNumber_ReturnsNull()
+        {
+            var combiner = CombineNumber.GetCombiner();
+            var retvalue = combiner.Combine(new int[] { -1, 23, 4, 5 });
+            Assert.IsNull(retvalue);
+        }
+        [TestMethod]
+        public void ForEmptyArray_FindCombinedNumber_ReturnsNull()
+        {
+            var combiner = CombineNumber.GetCombiner();
+            var retvalue = combiner.Combine(new int[] { });
+            Assert.IsNull(retvalue);
+        }
+        [TestMethod]
+        public void ForNonNegativeNumbers_FindCombinedNumber_ReturnsNonEmptyString()
+        {
+            var combiner = CombineNumber.GetCombiner();
+            var retvalue = combiner.Combine(new int[] { 1, 2, 35, 122 });
+            Assert.IsInstanceOfType(retvalue, typeof(string));
+            Assert.IsTrue(retvalue != "");
+        }
+        [TestMethod]
+        public void ForNonNegativeNumbers_FindCombinedNumber_ReturnsLargestCombinedNumber()
+        {
+            var combiner = CombineNumber.GetCombiner();
+            var retvalue = combiner.Combine(new int[] { 22, 220, 22, 122 });
+            Assert.IsTrue(retvalue != "");
+        }
+    }
+
+    public class CombineNumber
+    {
+        public static CombineNumber GetCombiner()
+        {
+            return new CombineNumber();
+        }
+
+        public string Combine(int[] v)
+        {
+            List<string> templist = new List<string>();
+            string retvalue = "";
+            if (IsNullOrEmpty(v) || IsContaningNegativeOrZeroNumber(v))
+            {
+                return null;
+            }
+            else
+            {
+                templist.AddRange(v.Select(x => x.ToString()).ToArray());
+                templist.Sort();
+                templist.Reverse();
+                foreach (var a in templist)
+                {
+                    retvalue += a;
+                }
+                return retvalue;
+            }
+        }
+
+        private static bool IsNullOrEmpty(int[] v)
+        {
+            return v == null || v.Count() == 0;
+        }
+
+        private bool IsContaningNegativeOrZeroNumber(int[] ar)
+        {
+            foreach (var item in ar)
+            {
+                if (item <= 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+    }
+}
---
+++ TDD/StringCalculatorTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        [TestMethod]
+        public void ForEmptyString_Add_ReturnsZero()
+        {
+            var monkey = StringCalculator.GetCalculator();
+            var retvalue = monkey.Add("");
+            Assert.AreEqual(0, retvalue);
+        }
+        [TestMethod]
+        public void ForSingleNumber_Add_ReturnsValue()
+        {
+            var monkey = StringCalculator.GetCalculator();
+            var retvalue = monkey.Add("1");
+            Assert.AreEqual(1, retvalue);
+        }
+    }
+
+    public class StringCalculator
+    {
+        public static StringCalculator GetCalculator()
+        {
+            return new StringCalculator();
+        }
+
+        public int Add(string v)
+        {
+            if (v == "")
+            {
+                return 0;
+            }
+            else
+            {
+                return Convert.ToInt32(v);
+            }
+        }
+    }
+}
